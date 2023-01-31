9e2754ddfd8568f27fef4c7205363abeb8c8598d
Parkjju <rudwns3927@gmail.com>
2023-01-31 17:43:15 +0900

    feat: RootViewController 스캐너 인스턴스 세그웨이 연결, 델리게이트 처리

--- barcode-scanner/ViewController.swift
+++ barcode-scanner/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scanResultLabel: UILabel!
+    @IBOutlet weak var scanButton: UIButton!
+    
+    let scannerViewController = ScannerViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        scannerViewController.delegate = self
     }
-
+    
+    
+    @IBAction func scanButtonTapped(_ sender: UIButton) {
+        self.navigationController?.pushViewController(scannerViewController, animated: true)
+    }
+    
 
 }
 
+extension ViewController: ScannerViewDelegate{
+    func didFindScannedText(text: String) {
+        scanResultLabel.text = text
+    }
+}
