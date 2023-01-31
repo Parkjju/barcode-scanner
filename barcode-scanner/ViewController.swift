e4726d7a0049882b3797aaa42df75b30e70d51fd
Parkjju <rudwns3927@gmail.com>
2023-01-31 15:15:19 +0900

    Initial Commit

---
+++ barcode-scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  barcode-scanner
+//
+//  Created by 박경준 on 2023/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
